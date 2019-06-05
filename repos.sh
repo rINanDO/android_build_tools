6a487d824e21b27048c59c5f28bb36b4ef1e2c77
Dominggoes Isakh <drjisakh@gmail.com>
2019-06-05 21:15:06 +0200

    Rebase android_hardware_samsung

--- repos.sh
+++ repos.sh
@@ -6,7 +6,7 @@ PROJECTS=(
 'vendor/samsung			proprietary_vendor_samsung		.	.		rinando	lineage-16.0	checkout'
 'device/samsung/i9100		android_device_samsung_i9100		.	.	 	rinando	lineage-16.0	checkout'
 'device/samsung/galaxys2-common	android_device_samsung_galaxys2-common	.	.	 	rinando	lineage-16.0	checkout'
-'hardware/samsung		android_hardware_samsung		.	.	 	rinando	lineage-16.0	checkout'
+'hardware/samsung		android_hardware_samsung		github	lineage-16.0 	rinando	lineage-16.0	rebase'
 'kernel/samsung/smdk4412	android_kernel_samsung_smdk4412		.	.		rinando	lineage-16.0	checkout'
 'frameworks/base		android_frameworks_base			github	lineage-16.0 	rinando	lineage-16.0	rebase'
 'build/make			android_build				github	lineage-16.0 	rinando	lineage-16.0	rebase'
