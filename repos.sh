c5c97b60400d4c262200f53120bffcd37e517c30
Dominggoes Isakh <drjisakh@gmail.com>
2019-06-05 23:24:29 +0200

    Remove android_frameworks_base repository
    Removed the hack for frameworks_base now we set low_ram=true to
    avoid ColorFade-effect which caused our libMali.so to crash.

--- repos.sh
+++ repos.sh
@@ -5,7 +5,6 @@ CUSTOM_REPO_URL="https://www.github.com/rinando/"
 PROJECTS=(
 'vendor/samsung			proprietary_vendor_samsung		.	.		rinando	lineage-16.0	checkout'
 'hardware/samsung		android_hardware_samsung		github	lineage-16.0 	rinando	lineage-16.0	rebase'
-'frameworks/base		android_frameworks_base			github	lineage-16.0 	rinando	lineage-16.0	rebase'
 'build/make			android_build				github	lineage-16.0 	rinando	lineage-16.0	rebase'
 )
 
