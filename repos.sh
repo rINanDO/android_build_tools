078cb3a01b5f0c74fa67df2b4cfd43e84e5827b9
Dominggoes Isakh <drjisakh@gmail.com>
2019-06-11 23:14:55 +0200

    Add missed repos

--- repos.sh
+++ repos.sh
@@ -6,5 +6,8 @@ PROJECTS=(
 'vendor/samsung			https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-16.0	https://github.com/rinando/proprietary_vendor_samsung	rinando	lineage-16.0	rebase'
 'hardware/samsung		https://github.com/rinando/android_hardware_samsung		github	lineage-16.0 	https://github.com/rinando/android_hardware_samsung	rinando	lineage-16.0	rebase'
 'build/make			https://github.com/rinando/android_build			github	lineage-16.0 	https://github.com/rinando/android_build	rinando	lineage-16.0	rebase'
+'frameworks/base		https://github.com/lineageos/android_frameworks_base		github  lineage-16.0 	https://github.com/rinando/android_frameworks_base		rinando  lineage-16.0	rebase'
+'device/samsung/galaxys2-common		https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-16.0 	.	.	.	checkout'
+'device/samsung/i9100			https://github.com/rinando/android_device_samsung_i9100			rinando	lineage-16.0 	.	.	.	checkout'
 )
 
