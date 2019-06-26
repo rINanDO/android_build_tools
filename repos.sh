1cb76ad1af132f9571985808d499a16b546f1ddf
Dominggoes Isakh <drjisakh@gmail.com>
2019-06-26 06:59:35 +0200

    Fix checkout i9100 repos

--- repos.sh
+++ repos.sh
@@ -6,7 +6,7 @@ PROJECTS=(
 'vendor/samsung			https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-16.0	https://github.com/rinando/proprietary_vendor_samsung	rinando	lineage-16.0	rebase'
 'hardware/samsung		https://github.com/rinando/android_hardware_samsung		github	lineage-16.0 	https://github.com/rinando/android_hardware_samsung	rinando	lineage-16.0	rebase'
 'frameworks/base		https://github.com/lineageos/android_frameworks_base		github  lineage-16.0 	https://github.com/rinando/android_frameworks_base		rinando  lineage-16.0	rebase'
-'device/samsung/galaxys2-common		https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-16.0 	.	.	.	checkout'
-'device/samsung/i9100			https://github.com/rinando/android_device_samsung_i9100			rinando	lineage-16.0 	.	.	.	checkout'
+'device/samsung/galaxys2-common		https://github.com/rinando/android_device_samsung_galaxys2-common	github	lineage-16.0 	https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-16.0	checkout'
+'device/samsung/i9100			https://github.com/rinando/android_device_samsung_i9100			github	lineage-16.0 	https://github.com/rinando/android_device_samsung_i9100			rinando	lineage-16.0	checkout'
 )
 
