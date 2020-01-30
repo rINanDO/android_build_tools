e1f11e632886b789a06f365a19908239f3d3f96f
Dominggoes Isakh <drjisakh@gmail.com>
2020-01-30 23:40:39 +0100

    Add repo's for i9300

--- repos.sh
+++ repos.sh
@@ -3,14 +3,16 @@ SOURCE_REPO_URL="https://www.github.com/lineageos"
 CUSTOM_REPO_URL="https://www.github.com/rinando/"
 
 PROJECTS=(
-'vendor/samsung			https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-17.1	https://github.com/rinando/proprietary_vendor_samsung	rinando	lineage-17.1	rebase'
-'hardware/samsung		https://github.com/lineageos/android_hardware_samsung		github	lineage-17.1 	https://github.com/rinando/android_hardware_samsung	rinando	lineage-17.1	rebase'
-'device/samsung/galaxys2-common		https://github.com/lineageos/android_device_samsung_galaxys2-common	github	lineage-17.1 	https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-17.1	rebase'
-'device/samsung/i9100			https://github.com/lineageos/android_device_samsung_i9100			github	lineage-17.1 	https://github.com/rinando/android_device_samsung_i9100			rinando	lineage-17.1	rebase'
-'hardware/ril				https://github.com/lineageos/android_hardware_ril				github	lineage-17.1 	https://github.com/rinando/android_hardware_ril			rinando	lineage-17.1	rebase'
-'kernel/samsung/smdk4412				https://github.com/lineageos/android_kernel_samsung_smdk4412				github	lineage-17.1 	https://github.com/rinando/android_kernel_samsung_smdk4412			rinando	lineage-17.1	rebase'
-'build/make				https://github.com/lineageos/android_build				github	lineage-17.1 	https://github.com/rinando/android_build			rinando	lineage-17.1	rebase'
-'bionic				https://github.com/lineageos/android_bionic				github	lineage-17.1 	https://github.com/rinando/android_bionic			rinando	lineage-17.1	rebase'
-
+'frameworks/base	https://github.com/lineageos/android_frameworks_base	github	lineage-17.1 	https://github.com/rinando/android_frameworks_base	rinando	lineage-17.1	rebase'
+'vendor/samsung	https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-17.1	https://github.com/rinando/proprietary_vendor_samsung	rinando	lineage-17.1	rebase'
+'hardware/samsung	https://github.com/lineageos/android_hardware_samsung	github	lineage-17.1 	https://github.com/rinando/android_hardware_samsung	rinando	lineage-17.1	rebase'
+'device/samsung/galaxys2-common	https://github.com/lineageos/android_device_samsung_galaxys2-common	github	lineage-17.0 	https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-17.1	checkout'
+'device/samsung/i9100	https://github.com/lineageos/android_device_samsung_i9100	github	lineage-17.0 	https://github.com/rinando/android_device_samsung_i9100	rinando	lineage-17.1	checkout'
+'device/samsung/smdk4412-common	https://github.com/lineageos/android_device_samsung_smdk4412-common	github	cm-14.1	https://github.com/rinando/android_device_samsung_smdk4412-common	rinando	lineage-17.1	checkout'
+'device/samsung/i9300	https://github.com/lineageos/android_device_samsung_i9300	github	cm-14.1 	https://github.com/rinando/android_device_samsung_i9300	rinando	lineage-17.1	checkout'
+'hardware/ril	https://github.com/lineageos/android_hardware_ril	github	lineage-17.1 	https://github.com/rinando/android_hardware_ril	rinando	lineage-17.1	rebase'
+'kernel/samsung/smdk4412	https://github.com/lineageos/android_kernel_samsung_smdk4412	github	lineage-17.0 	https://github.com/rinando/android_kernel_samsung_smdk4412			rinando	lineage-17.1	checkout'
+'build/make	https://github.com/lineageos/android_build	github	lineage-17.1 	https://github.com/rinando/android_build	rinando	lineage-17.1	rebase'
+'bionic	https://github.com/lineageos/android_bionic	github	lineage-17.1 	https://github.com/rinando/android_bionic	rinando	lineage-17.1	rebase'
 )
 
